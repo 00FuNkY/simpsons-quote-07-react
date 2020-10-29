2c5d950c66322bf1a4f9d24246f27d79308f606c
Malo Pivert <pivertmalo@gmail.com>
2020-10-29 10:26:33 +0100

    resolved undefined props and its now working fine

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Axios from "axios";
+import QuoteCard from "./components/QuoteCard";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const sampleQuotes = {
+  quote: "Ha ha!",
+  character: "Nelson Muntz",
+  image:
+    "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FNelsonMuntz.png?1497567511185",
+};
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      simpsonState: sampleQuotes,
+    };
+    this.getQuote = this.getQuote.bind(this);
+  }
+
+  getQuote() {
+    Axios.get("https://thesimpsonsquoteapi.glitch.me/quotes")
+      .then((response) => response.data)
+      .then((data) => {
+        this.setState({
+          simpsonState: data[0],
+        });
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <QuoteCard quote={this.state.simpsonState} />
+        <button type="button" onClick={this.getQuote}>
+          Get QUOTE
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
